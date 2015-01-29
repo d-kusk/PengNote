898ab6ab1642800bc6056c7d70150bd00654c56d
小西 大祐 <skydai1151@gmail.com>
2015-01-30 02:16:54 +0900

    各phpファイルの作成

---
+++ front-page.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <title>PengNote</title>
+    <meta content="" name="keywords" />
+    <meta content="" name="description" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <link href="css/style.css" rel="stylesheet" />
+    <!--[if lt IE 9]
+    script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
+    script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"--><!--[endif]-->
+  </head>
+  <body>
+    <header class="l-global-header">
+      <div class="l-logo-area">
+        <h1 class="logo">
+          <a href="/"><img alt="PengNote" height="55" src="images/logo.png" width="238" /></a>
+        </h1>
+        <p>
+          勉強した事や行った場所の感想を書くブログ
+        </p>
+      </div>
+      <nav class="l-global-nav">
+        <ul class="l-global-nav-contents">
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="l-contents">
+      <article class="l-entry-area">
+        <a class="l-eyecatch-area" href="#"><img alt="アイキャッチ" height="279" src="images/thumbnail.png" width="450" /></a>
+        <div class="l-entry-content">
+          <ul class="entry-data-lists">
+            <li class="postedtime">
+              <time datetime="2015-01-09" pubdate="pubdate">2015-01-09</time>
+            </li>
+            <li class="category">
+              <a href="#">カテゴリー</a>
+            </li>
+          </ul>
+          <h2 class="entry-title">
+            <a href="#">エントリータイトル</a>
+          </h2>
+        </div>
+      </article>
+      <article class="l-entry-area">
+        <a class="l-eyecatch-area" href="#"><img alt="アイキャッチ" height="279" src="images/thumbnail.png" width="450" /></a>
+        <div class="l-entry-content">
+          <ul class="entry-data-lists">
+            <li class="postedtime">
+              <time datetime="2015-01-09" pubdate="pubdate">2015-01-09</time>
+            </li>
+            <li class="category">
+              <a href="#">カテゴリー</a>
+            </li>
+          </ul>
+          <h2 class="entry-title">
+            <a href="#">エントリータイトル</a>
+          </h2>
+        </div>
+      </article>
+      <nav class="l-archives-pager">
+        <span class="page-numbers current">1</span><a class="page-numbers" href="http://peng-note.com/page/2">2</a><a class="page-numbers" href="http://peng-note.com/page/2">3</a><a class="page-numbers" href="http://peng-note.com/page/2">4</a><a class="page-numbers" href="http://peng-note.com/page/2">5</a><span class="page-numbers dots">…</span><a class="page-numbers" href="http://peng-note.com/page/17">17</a><a class="next page-numbers" href="http://peng-note.com/page/2">» </a>
+      </nav>
+    </main>
+    <footer class="l-global-footer">
+      <div class="l-footer-contents">
+        <div class="l-footer-widget">
+          <h2>
+            TagCloud
+          </h2>
+          <ul>
+            <li>
+              <a class="tag" href="#">タグ1</a>
+              <a class="tag" href="#">タグ2</a>
+              <a class="tag" href="#">タグ3</a>
+              <a class="tag" href="#">タグ4</a>
+              <a class="tag" href="#">タグ5</a>
+              <a class="tag" href="#">タグ6</a>
+              <a class="tag" href="#">タグ7</a>
+            </li>
+          </ul>
+        </div>
+        <div class="l-footer-widget">
+          <h2>
+            Author
+          </h2>
+          <div class="author-content">
+            <img alt="Authorの画像" height="100" src="images/profile_icon.gif" width="100" />
+            <p>
+              大阪在住の大学生くうすけです。チョコと布団とWordPressが好き。Webデザイン・コーディングの勉強中です。<a href="#">詳しくはこちら</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <p class="copy">
+        <small>&copy; 2015 PengNote</small>
+      </p>
+    </footer>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/jquery.fs.naver.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
---
+++ page.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <title>PengNote</title>
+    <meta content="" name="keywords" />
+    <meta content="" name="description" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <link href="css/style.css" rel="stylesheet" />
+    <!--[if lt IE 9]
+    script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
+    script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"--><!--[endif]-->
+  </head>
+  <body>
+    <header class="l-global-header">
+      <div class="l-logo-area">
+        <h1 class="logo">
+          <a href="/"><img alt="PengNote" height="55" src="images/logo.png" width="238" /></a>
+        </h1>
+        <p>
+          勉強した事や行った場所の感想を書くブログ
+        </p>
+      </div>
+      <nav class="l-global-nav">
+        <ul>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+          <li>
+            <a href="#">ナビゲーション</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="l-contents">
+      <article class="l-entry-area">
+        <header class="entry-header">
+          <h2 class="entry-title">
+            エントリータイトル
+          </h2>
+        </header>
+        <div class="entry-body">
+          <img alt="アイキャッチ" class="page-eyecatch" height="279" src="images/thumbnail.png" width="450" />
+          <h2>
+            見出し
+          </h2>
+          <p></p>
+          親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。<blockquote>弱虫やーい。と囃したからである。</blockquote>
+          <p></p>
+          小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、
+          <h3>
+            この次は抜かさずに飛んで見せますと答えた。（青空文庫より）
+          </h3>
+          <ul>
+            <li>
+              親譲りの無鉄砲で小供の時から損ばかりしている。
+            </li>
+            <li>
+              小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。
+            </li>
+          </ul>
+          <p>
+            なぜそんな無闇をしたと聞く人があるかも知れぬ。
+          </p>
+          <ol>
+            <li>
+              別段深い理由でもない。
+            </li>
+            <li>
+              弱虫やーい。
+            </li>
+          </ol>
+          <p>
+            と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから.
+          </p>
+        </div>
+      </article>
+    </main>
+    <footer class="l-global-footer">
+      <div class="l-footer-contents">
+        <div class="l-popular-posts">
+          <h2>
+            Popular Posts
+          </h2>
+          <ol>
+            <li>
+              <img alt="アイキャッチ" height="70" src="images/thumbnail.png" width="70" /><a href="#">エントリータイトル</a>
+            </li>
+            <li>
+              <img alt="アイキャッチ" height="70" src="images/thumbnail.png" width="70" /><a href="#">エントリータイトル</a>
+            </li>
+            <li>
+              <img alt="アイキャッチ" height="70" src="images/thumbnail.png" width="70" /><a href="#">エントリータイトル</a>
+            </li>
+          </ol>
+        </div>
+        <div class="l-category-lists">
+          <h2>
+            Category
+          </h2>
+          <ul>
+            <li>
+              <a href="#">カテゴリー1</a>
+            </li>
+            <li>
+              <a href="#">カテゴリー2</a>
+            </li>
+            <li>
+              <a href="#">カテゴリー3</a>
+            </li>
+            <li>
+              <a href="#">カテゴリー4</a>
+            </li>
+            <li>
+              <a href="#">カテゴリー5</a>
+            </li>
+            <li>
+              <a href="#">カテゴリー6</a>
+            </li>
+            <li>
+              <a href="#">カテゴリー7</a>
+            </li>
+          </ul>
+        </div>
+        <div class="l-author">
+          <h2>
+            Author
+          </h2>
+          <div class="l-author-contents">
+            <img alt="Authorの画像" height="100" src="images/profile_icon.gif" width="100" />
+            <p>
+              大阪在住の大学生くうすけです。チョコと布団とWordPressが好き。Webデザイン・コーディングの勉強中です。<a href="#">詳しくはこちら</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <p class="copy">
+        <small>&copy; 2015 PengNote</small>
+      </p>
+    </footer>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/jquery.fs.naver.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
