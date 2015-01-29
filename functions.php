3fcfea9e3620b87ccde4e48accd73ffcd0602ec1
小西 大祐 <skydai1151@gmail.com>
2015-01-30 04:14:21 +0900

    ウィジェット機能の有効化と設定

--- functions.php
+++ functions.php
@@ -2,3 +2,14 @@
 // カスタムメニューを有効化
 add_theme_support( 'menus' );
 register_nav_menu( 'place_global', 'グローバルナビゲーション' );
+
+function footer_widgets_init() {
+  register_sidebar(array(
+   'name' => 'フッターウィジェット',
+   'description' => 'フッターに置くウィジェット',
+   'id' => 'footer-widget',
+   'before_widget' => '<div class="l-footer-widget">',
+   'after_widget' => '</div>',
+  ));
+}
+add_action( 'widgets_init', 'footer_widgets_init' );
