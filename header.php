24073a817c10a955a0eaf4fcca1b6f68915f6587
Daisuke <skydai1151@gmail.com>
2015-07-27 18:14:36 +0900

    homeのURLを出力するコードを置き換えた

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
     <header class="l-global-header">
       <div class="l-logo-area">
         <h1 class="logo">
-          <a href="<?php bloginfo('url'); ?>"><img alt="PengNote" height="55" src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" width="238" /></a>
+          <a href="<?php echo esc_url( home_url() ); ?>"><img alt="PengNote" height="55" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logo.svg" width="238" /></a>
         </h1>
         <p><?php bloginfo('description'); ?></p>
       </div>
@@ -45,13 +45,13 @@
     </header>
     <?php if(is_single()): ?>
     <ul class="l-breadcrumbs">
-      <li><a href="<?php echo get_option('home'); ?>">HOME</a>&gt;</li>
+      <li><a href="<?php echo esc_url( home_url() ); ?>">HOME</a>&gt;</li>
       <li><a href="<?php $cat = get_the_category(); $cat = $cat[0]; echo get_category_link( $cat->term_id ); ?>"><?php echo $cat->cat_name; ?></a>&gt;</li>
       <li class="current"><?php the_title(''); ?></li>
     </ul>
     <?php elseif(is_page()): ?>
     <ul class="l-breadcrumbs">
-      <li><a href="<?php echo get_option('home'); ?>">HOME</a>&gt;</li>
+      <li><a href="<?php echo esc_url( home_url() ); ?>">HOME</a>&gt;</li>
       <?php foreach ( array_reverse(get_post_ancestors($post->ID)) as $parid ) { ?>
       <li><a href="<?php echo get_page_link( $parid );?>" title="<?php echo get_page($parid)->post_name; ?>">
       <?php echo get_page($parid)->post_name; ?></a>&gt;</li>
